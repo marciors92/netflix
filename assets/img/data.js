7d9d1dcbc61748411398a57efe255393ea2f1240
Majisto <marciors92@gmail.com>
2026-04-01 14:12:24 -0300

    feat(data): estrutura categorias e vincula trailer do YouTube
    Configura o array de categorias com novos títulos, badges personalizados e define o link do trailer padrão para todos os itens.

---
+++ assets/img/data.js
@@ -0,0 +1,29 @@
+export const categories = [
+    {
+        title: "Épicos",
+        items: [
+            { img: "/assets/img/epico.01.jpg", top10: true, badge: "Nacional", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/epico.02.jpg", top10: true, badge: "Magia", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/epico.03.jpg", badge: "Aventura", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/epico.04.jpg", top10: true, badge: "Fantasia", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "/assets/img/serie.01.jpg", top10: true, badge: "Original", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/serie.02.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/serie.03.jpg", badge: "Terror/Comédia", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/serie.04.jpg", badge: "Clássico", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "/assets/img/maratonar.01.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/maratonar.02.jpg", top10: true, badge: "Ação", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/maratonar.03.jpg", top10: true, badge: "Aventura", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+            { img: "/assets/img/maratonar.04.jpg", top10: true, badge: "Animação", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=GV3HUDMQ-F8" },
+        ]
+    }
+];
